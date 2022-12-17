f69bc60e70b6bf253eca336c77070cc9c74060ba
Anurag Tyagi <anuragtyagi014@gmail.com>
2022-12-17 16:50:58 +0530

    simple admin theme with headre footer includes

---
+++ table.php
@@ -0,0 +1,60 @@
+<?php include("inc/header.php");?>
+      <div class="container-fluid page-body-wrapper">
+        <?php include("inc/sidebar.php");?>
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <div class="page-header">
+              <h3 class="page-title"> Basic Tables </h3>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#">Tables</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Basic tables</li>
+                </ol>
+              </nav>
+            </div>
+            <div class="row">
+              <div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title">Hoverable Table</h4>
+                        <p class="card-description"> Add class <code>.table-hover</code>
+                    </p>
+                    <table class="table table-hover">
+                      <thead>
+                        <tr>
+                          <th>User</th>
+                          <th>Product</th>
+                          <th>Sale</th>
+                          <th>Status</th>
+						  <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Jacob</td>
+                          <td>Photoshop</td>
+                          <td class="text-danger"> 28.76% <i class="mdi mdi-arrow-down"></i></td>
+                          <td><label class="badge badge-danger">Pending</label></td>
+						  <td>
+						  <a href="" class="btn btn-secondary btn-sm"><i class="mdi mdi-eye"></i></a>
+						  <a href="" class="btn btn-success btn-sm"><i class="mdi mdi-pencil"></i></a>
+						  <a href="" class="btn btn-danger btn-sm"><i class="mdi mdi-delete"></i></a>
+						  </td>
+                        </tr>
+                        
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer class="footer">
+            <div class="container-fluid d-flex justify-content-between">
+              <span class="text-muted d-block text-center text-sm-start d-sm-inline-block">Copyright © bootstrapdash.com 2021</span>
+              <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap admin template</a> from Bootstrapdash.com</span>
+            </div>
+          </footer>
+        </div>
+      </div>
+<?php include("inc/footer.php");?>
